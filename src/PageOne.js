a3b29a8293934869b3b50bd8fc202b04e0230d25
Andrew <andrewkim1998@gmail.com>
2023-02-14 17:28:29 -0600

    angel list no render bug

--- src/PageOne.js
+++ src/PageOne.js
@@ -22,7 +22,7 @@ export const PageOne = () => {
                     <a href="https://github.com/adotk24" target="_blank" rel="noreferrer"><img className="git-hub" id="page-one-icons" src={gitHub} alt="" /></a>
                     <a href="https://www.linkedin.com/in/andrewkimcode/" target="_blank" rel="noreferrer"><img className="linked-in" id="page-one-icons" src={linkedIn} alt="" /></a>
                     <a href="https://angel.co/u/andrew-k-26" target="_blank" rel="noreferrer"><img className="angel-list" id="page-one-icons" src={angelList} alt="" /></a>
-                    <a href='./MyResume.pdf' target='_blank' refl='noreferrer'><img className="angel-list" id="page-one-icons" src={resume} alt="" /></a>
+                    <a href='./MyResume.pdf' target='_blank' rel='noreferrer'><img className="angel-list" id="page-one-icons" src={resume} alt="" /></a>
                 </div>
             </div>
         </div >
